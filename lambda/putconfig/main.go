7cabd1d90491a0e2f69cd97ea6598471e7acfafb
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2024-02-01 09:21:00 +0100

    Simplify response creation

--- lambda/putconfig/main.go
+++ lambda/putconfig/main.go
@@ -19,35 +19,28 @@ func main() {
 func handler(r events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	item := dynamo.ConfigItem{}
 	err := json.Unmarshal([]byte(r.Body), &item)
-	log.Printf("config item %v", item)
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusBadRequest,
-			Headers: map[string]string{
-				"Access-Control-Allow-Origin":      "*",
-				"Access-Control-Allow-Credentials": "true",
-			},
-		}
+		log.Printf("error: %v", err)
+		return buildResponse(http.StatusBadRequest)
 	}
 
 	err = dynamo.AddConfigItem(item)
 	if err != nil {
 		log.Printf("error: %v", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Headers: map[string]string{
-				"Access-Control-Allow-Origin":      "*",
-				"Access-Control-Allow-Credentials": "true",
-			},
-		}
+		return buildResponse(http.StatusInternalServerError)
 	}
 
+	log.Printf("created or updated config item %v", item)
+	return buildResponse(http.StatusNoContent)
+}
+
+func buildResponse(httpStatus int) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{
-		Body:       "https://77srys74sh.execute-api.eu-central-1.amazonaws.com/dev/jobs",
-		StatusCode: http.StatusOK,
+		StatusCode: httpStatus,
 		Headers: map[string]string{
 			"Access-Control-Allow-Origin":      "*",
 			"Access-Control-Allow-Credentials": "true",
 		},
 	}
+
 }
